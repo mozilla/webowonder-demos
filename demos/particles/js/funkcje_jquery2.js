b33c4342e8590ae237ea56a1ee135d0fcb2e238b
Austin King <shout@ozten.com>
2011-01-11 11:43:15 -0800

    Adding skeleton of the particles demo as an example. Added couple resources of each kind

---
+++ demos/particles/js/funkcje_jquery2.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 		
+
+
+					<!-- blysk na obrazkach -->
+					$(".img img").fadeTo("fast", .8);					
+						$(".img img").hover(function(){
+															$(this).fadeTo("fast", 1);
+													},function(){
+															$(this).fadeTo("fast", .8);
+													});
+											
+											
+	
+ });
+ 

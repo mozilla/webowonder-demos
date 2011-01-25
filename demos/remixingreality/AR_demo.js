5a2676429029884acc429b4eaf1b6fe8e28e4cf2
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2011-01-25 07:21:42 +0200

    demos/remixingreality: ball by default, turnAROff() & turnAROn() functions to use from hypothesized two-state AR toggle button

--- demos/remixingreality/AR_demo.js
+++ demos/remixingreality/AR_demo.js
@@ -133,7 +133,7 @@
     display2.scene.appendChild(ball);
     display2.scene.appendChild(ballShadow);
   }
-  ballOn = false;
+  ballOn = true;
   toggleBall = function() {
     ballOn = !ballOn;
   }
@@ -215,7 +215,7 @@
     var ctx = canvas.getContext('2d');
     ctx.font = "24px URW Gothic L, Arial, Sans-serif";
 
-    currentElem = byClass('content')[0];
+    currentElem = byClass('content')[4];
 
     setImage = function(elem) {
       images.forEach(function(img){ img.setImage(elem); });
@@ -456,11 +456,18 @@
 
     toggleAR = function(){
       if (video.style.display == 'none') {
-        video.style.display = 'inline-block';
-        gcanvas.style.display = 'none';
+        turnAROff();
       } else {
-        video.style.display = 'none';
-        gcanvas.style.display = 'inline-block';
+        turnAROn();
       }
+    }
+
+    turnAROn = function() {
+      video.style.display = 'none';
+      gcanvas.style.display = 'inline-block';
+    }
+    turnAROff = function() {
+      video.style.display = 'inline-block';
+      gcanvas.style.display = 'none';
     }
   }

c0389504fd1f3ed00f3977705b4c5575aa69b5d3
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2011-01-26 11:21:55 +0200

    demos/remixingreality/AR_demo.js: fancier audio visualizer, move vis to other side so that hand does not obstruct it

--- demos/remixingreality/AR_demo.js
+++ demos/remixingreality/AR_demo.js
@@ -13,25 +13,48 @@
     return flip ? Math.PI-a : Math.PI+a;
   }
 
+  interpolate = function(a,b,f,r) {
+    if (r == null) r = [];
+    for (var i=0; i<a.length; i++)
+      r[i] = a[i] * (1-f) + b[i] * f;
+    return r;
+  }
+
   createCubes = function(container){
     var bdcubemodel = new Magi.Node();
     var bdcubes = [];
-    var cubemat = material.copy();
-    cubemat.floats.MaterialAmbient[3] = 0.5;
     for (var i=-5; i<5; i++) {
       for (var j=-5; j<5; j++) {
         var c = new Magi.Cube();
         c.setPosition(i+0.5,j+0.5,0);
-        c.material = cubemat;
+        c.material = material.copy();
+        var f = (i+5)/10;
+        var a = interpolate([0,0.5,1,1], [1,0,0,1], f);
+        c.material.floats.MaterialAmbient[3] = 0.5;
+        c.material.floats.MaterialDiffuse = a;
+        c.material.floats.MaterialDiffuse[3] = 0.35;
+        c.depthMask = false;
+        var glowy = new Magi.Cube();
+        glowy.material = c.material.copy();
+        glowy.material.floats.MaterialDiffuse[3] = 1;
+//         glowy.material.floats.MaterialEmit = a.map(function(i){ return i / 6; });
+        glowy.material.floats.MaterialAmbient[3] = 0.9;
+        glowy.setScale(0.8);
+        glowy.setZ(0.1);
+        glowy.setAxis(1,0,0).setAngle(j*0.1);
+        c.setAxis(1,1,1).setAngle(j*0.1);
+        c.appendChild(glowy);
         bdcubes.push(c);
         bdcubemodel.appendChild(bdcubes.last());
       }
     }
-    bdcubemodel.setScale(1/5, 1/10, 1/5);
-    bdcubemodel.setY(-1.5);
+    bdcubemodel.setScale(1/5, 1/12, 1/5);
+    bdcubemodel.setX(+(1+5/10));
+    bdcubemodel.setY(-0.05);
+    bdcubemodel.setAngle(-Math.PI/2).setAxis(0,0,1);
     bdcubemodel.addFrameListener(function(t,dt){
       updateVU(bdcubes);
-      this.display = !audioTag.paused;
+      this.display = !(audioTag.paused || audioTag.currentTime == audioTag.duration);
     });
     container.appendChild(bdcubemodel);
   }

19414b85cb7db52dfe7257f95f66217b061e0242
louisremi <louisremi@louisremi-laptop.(none)>
2011-06-14 19:32:45 +0200

    Remove useless CORS headers

--- demos/friends timeline/sse.php
+++ demos/friends timeline/sse.php
@@ -2,8 +2,6 @@
 
 // Send appropriate mime type
 header("Content-Type: text/event-stream\n\n");
-// Allow Cross-Origin XmlHTTPRequest
-header("Access-Control-Allow-Origin: *\n\n");
 
 // Parse messages
 $filename = "timelineEvents.txt";
@@ -13,9 +11,9 @@ fclose($handle);
 
 $messages = preg_split( "/\n\n/", $contents );
 
-// Send a new message very 10 to 30
+// Send a new message very 2 to 7 seconds
 foreach ( $messages as $message ) {
-	sleep( rand(2,7) );
+	sleep( rand(2, 7) );
 	echo $message;
 	echo "\n\n";
 
